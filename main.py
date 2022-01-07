6dc0cd83d83cf9ef990d4d6b2d79b5522bf4d4a8
Patrick Andresen <patrickand23@gmail.com>
2022-01-07 15:53:38 -0600

    Cleanup

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ def run():
     pipeline_options = PipelineOptions(
         project='york-cdf-start',
         region='us-central1',
-        temp_location='gs://pas_df_bucket/dataflow/tmp/',
-        staging_location='gs://pas_df_bucket/dataflow/staging/',
+        temp_location='gs://york_temp_files/tmp',
+        staging_location='gs://york_temp_files/staging',
         save_main_session=True,
         job_name='patrick-andresen-final-job'
     )
